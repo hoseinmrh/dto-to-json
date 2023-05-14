628602b6cb643f18aa57c10b53ee2f3e109739f3
Hosein Mirhoseini <hoseinmirhoseini64@gmail.com>
2023-05-15 00:15:24 +0330

    :tada: Init

---
+++ JSONcreate.py
@@ -0,0 +1,23 @@
+file_data = []
+# Put your file here
+file = open("file.txt" , "r")
+lines = file.readlines()
+# Reading data
+for line in lines:
+    if("//" in line  or "@" in line or len(line) == 0): #Ignoring comments and Decorators and new lines
+        continue
+    if("exoprt class" in line or "{" in line or "}" in line): #Ignoring class name
+        continue
+
+    file_data.append(line.strip().split())
+
+
+for i in range(len(file_data)):
+    data = file_data[i]
+    if (len(data) > 0):
+        string = data[0]
+        string = string[:len(string)]
+        if(i == (len(file_data) - 1)):
+            print("\"%s\""% string, ":")
+        else:
+            print("\"%s\""% string, ":", ",")
